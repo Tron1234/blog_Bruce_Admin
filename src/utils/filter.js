04049b34d3b6d17e32602ee309ce7e235ba9ffe2
Tron1234 <47961959+Tron1234@users.noreply.github.com>
2022-02-22 17:46:45 +0800

    Update filter.js
    移除formatDate

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -2,26 +2,6 @@ const install = app => {
   app.config.globalProperties.$filter = {
     formatSex(value) {
       return Number(value) ? '女' : '男';
-    },
-    formatDate(date, fmt = "YYYY-mm-dd HH:MM") {
-      const time = new Date(date);
-      let ret;
-      const opt = {
-        "Y+": time.getFullYear().toString(), // 年
-        "m+": (time.getMonth() + 1).toString(), // 月
-        "d+": time.getDate().toString(), // 日
-        "H+": time.getHours().toString(), // 时
-        "M+": time.getMinutes().toString(), // 分
-        "S+": time.getSeconds().toString() // 秒
-        // 有其他格式化字符需求可以继续添加，必须转化成字符串
-      };
-      for (let k in opt) {
-        ret = new RegExp("(" + k + ")").exec(fmt);
-        if (ret) {
-          fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
-        };
-      };
-      return fmt;
     }
   }
 };
